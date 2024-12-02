7955438d61ce50cbccd2ac1e1ee618debfb176a0
Long H Le <longlh1809@gmail.com>
2024-12-02 03:27:35 +0000

    Modify gitignore

--- python-api/app/main.py
+++ python-api/app/main.py
@@ -1,10 +1,26 @@
 from fastapi import Depends, FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from .dependencies import get_query_token, get_token_header
 from .internal import admin
 from .routers import items, users
 
+
 app = FastAPI(dependencies=[Depends(get_query_token)])
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(users.router)
